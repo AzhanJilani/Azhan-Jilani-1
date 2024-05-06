078a8dc558d7eb30bcf9275f8055a5c1da33f554
AzhanJilani <169088645+AzhanJilani@users.noreply.github.com>
2024-05-06 23:46:54 +0500

    Add files via upload

---
+++ 04.ts
@@ -0,0 +1,5 @@
+let famous_person  = "Albert Einstein";
+let words = "Imagination is more important than knowledge.";
+let message = `"{words}" - {famous_person}`;
+
+console.log(message);
---
+++ 05.ts
@@ -0,0 +1,5 @@
+let nameWithWhitespace = "\t\n   John Doe \n\t";
+console.log("Name with whitespace:", nameWithWhitespace);
+
+let strippedName = nameWithWhitespace.trim();
+console.log("Stripped name:", strippedName);
